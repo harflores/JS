9f4d397de223d19baa9ea6e9bd691c9f80d0cab2
harflores <49909627+harflores@users.noreply.github.com>
2022-06-04 23:20:10 -0400

    Console y ESlint
    se realizan pruebas con console y se instala ES lint para mas adelante utilizar en analisis de codigo

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,3 +1,13 @@
+console.time('Inicio');
+
 // PREGUNTA AL USUARIO SU NOMBRE PARA DESPUES MOSTRARLO EN PAGINA
 const nombre = prompt("Cual es tu nombre?");
 document.querySelector('.contenido').innerHTML=`${nombre} esta aprendiendo JS`;
+
+
+console.log('Hola Mundo');
+console.error('Algo salio mal!');
+console.warn("Eso no esta permitido");
+
+console.time('Fin');
+
