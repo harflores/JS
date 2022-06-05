5507f1faa2b267f9a15383bdf519ee28423d16ef
harflores <49909627+harflores@users.noreply.github.com>
2022-06-04 22:39:27 -0400

    comentario
    Se añade comentario

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,2 +1,3 @@
+// PREGUNTA AL USUARIO SU NOMBRE PARA DESPUES MOSTRARLO EN PAGINA
 const nombre = prompt("Cual es tu nombre?");
 document.querySelector('.contenido').innerHTML=`${nombre} esta aprendiendo JS`;
